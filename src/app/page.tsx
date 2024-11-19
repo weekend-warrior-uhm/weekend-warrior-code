110629e1dcc50c0cbc1080edb1c2e57effbae69c
Mhar Olipani <mhar@hawaii.edu>
2024-11-19 09:20:06 -1000

    update page.tsx for demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,23 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
+    <div id="landing-page">
+      <div className="landing-green-background">
+        <Container fluid className="py-3 text-center">
+          <Row className="align-middle">
+            <Col xs={4}>
+              <Image src="next.svg" width="150px" alt="" />
+            </Col>
 
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
-      </Row>
-    </Container>
+            <Col xs={8} className="d-flex flex-column justify-content-center">
+              <h3 style={{ paddingBottom: '20px', color: 'white' }}>
+                What are your plans for the weekend?
+              </h3>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   </main>
 );
 
