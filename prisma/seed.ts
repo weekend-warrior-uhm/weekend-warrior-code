e63732a693d244713564d2d99fb638791c886dd8
Mhar Olipani <mhar@hawaii.edu>
2024-12-03 10:56:07 -1000

    Edit the schema.prisma, the seed.ts, and the setting.dev.json to work with the new implemented changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   console.log('Seeding the database');
-  const password = await hash('changeme', 10);
   config.defaultAccounts.forEach(async (account) => {
+    const hashedPassword = await hash(account.password, 10);
     let role: Role = 'USER';
     if (account.role === 'ADMIN') {
       role = 'ADMIN';
@@ -18,11 +18,15 @@ async function main() {
       update: {},
       create: {
         email: account.email,
-        password,
+        username: account.username,
+        password: hashedPassword,
+        fullName: account.fullName,
+        phone: account.phone,
+        gender: account.gender,
+        interests: account.interests,
         role,
       },
     });
-    // console.log(`  Created user: ${user.email} with role: ${user.role}`);
   });
   config.defaultData.forEach(async (data, index) => {
     console.log(`  Adding activity: ${data.name}`);
