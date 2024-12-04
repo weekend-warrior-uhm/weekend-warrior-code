cda7601f7970c8c0b71a7549ac7f34bd20ceac6f
Mhar Olipani <mhar@hawaii.edu>
2024-12-03 14:55:21 -1000

    Reapply "undo getserversideprops"
    This reverts commit 165b9cea86feee66d22dee75df508bee043a9c28.

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,36 +6,32 @@ import { prisma } from '@/lib/prisma';
 import { User } from '@prisma/client';
 import AddUser from '@/components/AddUser';
 
-const ListUsers = async () => {
-  /* This is for protecting the page so that only signed in users can access:
-
-  const session = await getServerSession(authOptions);
-  loggedInProtectedPage(
-    session as {
-      user: { email: string; id: string; randomKey: string };
-    } | null,
-  );
-  */
+export const getServerSideProps = async () => {
   const users: User[] = await prisma.user.findMany({});
-
-  return (
-    <main>
-      <Container id="list" fluid className="py-3">
-        <Row>
-          <Col>
-            <h2 className="text-center">Users</h2>
-            <Row xs={1} md={2} lg={3} className="g-4">
-              {users.map((user) => (
-                <Col key={user.id}>
-                  <AddUser user={user} />
-                </Col>
-              ))}
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </main>
-  );
+  return {
+    props: {
+      users,
+    },
+  };
 };
+
+const ListUsers = ({ users }: { users: User[] }) => (
+  <main>
+    <Container id="list" fluid className="py-3">
+      <Row>
+        <Col>
+          <h2 className="text-center">Users</h2>
+          <Row xs={1} md={2} lg={3} className="g-4">
+            {users.map((user) => (
+              <Col key={user.id}>
+                <AddUser user={user} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  </main>
+);
 
 export default ListUsers;
