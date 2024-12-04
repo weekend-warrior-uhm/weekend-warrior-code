1a97c22303729e1d637932f1741ab0c71fa2fb1b
Mhar Olipani <mhar@hawaii.edu>
2024-12-03 21:39:08 -1000

    added placeholder contact & support and safety reminders.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,25 @@
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="footer mt-auto py-3 bg-dark">
     <Container>
-      <Col className="text-center" style={{ color: 'white' }}>
-        Weekend Warrior
-        <br />
-        ICS 314 Final Project
-        <br />
-        <a style={{ color: 'white' }} href="https://weekend-warrior-uhm.github.io">
-          https://weekend-warrior-uhm.github.io
-        </a>
-      </Col>
+      <Row className="text-center" style={{ color: 'white' }}>
+        <Col>
+          <h5>Weekend Warrior</h5>
+          <p>ICS 314 Final Project</p>
+          <a style={{ color: 'white' }} href="https://weekend-warrior-uhm.github.io">
+            https://weekend-warrior-uhm.github.io
+          </a>
+        </Col>
+        <Col>
+          <h5>Quick Links</h5>
+          <ul className="list-unstyled">
+            <li><a style={{ color: 'white' }} href="/contact-support">Contact & Support</a></li>
+            <li><a style={{ color: 'white' }} href="/safety">Safety Reminders</a></li>
+          </ul>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
