99836cf34f088d83f8226e2d9c650fd6408a42cd
Sean Hiroki Flynn <sflynn7@hawaii.edu>
2024-12-12 11:43:45 -1000

    New community guidelines page

---
+++ src/app/community-guidelines/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const CommunityGuidelines = () => (
+  <main>
+    <Container className="py-5">
+      <h2>Community Guidelines</h2>
+
+      <section>
+        <ul>
+          <li>
+            Become familiar with the Emergency Call Boxes (ECBs) located around campus.
+            ECBs can be identified by a blue light mounted above the box.
+          </li>
+          <li>
+            Program the number for UHM DPS into your phone: (808) 956-6911. If you see something out of the ordinary,
+            call us!
+          </li>
+          <li>
+            Avoid situations that put you at greater risk (such as walking alone after dark or in high crime areas
+            around the city).
+          </li>
+          <li>
+            If you are being harassed, loudly say “Leave me alone!” If that doesn&apos;t stop the harassment,
+            continue to attract people&apos;s attention and head towards a store or other populated area.
+          </li>
+          <li>
+            Be aware of your surroundings while walking/jogging alone. If using headphones,
+            always leave one earbud out so that you can stay alert.
+          </li>
+          <li>
+            When you go to social gatherings, go with a group of friends. Arrive together,
+            check in with each other throughout the evening, and leave together.
+          </li>
+          <li>
+            Do not accept drinks (alcoholic or non-alcoholic) from strangers.
+          </li>
+          <li>
+            If you see anything unusual or suspicious, notify UHM Department of Public Safety.
+            Do not physically engage suspects involved in criminal activity. Stay at a safe distance,
+            note the suspect&apos;s description, and seek help as soon as possible.
+          </li>
+        </ul>
+      </section>
+    </Container>
+  </main>
+);
+
+export default CommunityGuidelines;
